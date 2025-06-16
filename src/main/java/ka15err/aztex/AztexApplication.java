c952b2f1ecf857fe9e7bff038ec4e0f0393bfff6
kLot2005 <bekars.bihimbai2005@gmail.com>
2025-06-16 22:40:06 +0500

    Initial commit

---
+++ src/main/java/ka15err/aztex/AztexApplication.java
@@ -0,0 +1,13 @@
+package ka15err.aztex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AztexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AztexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ka15err/aztex/AztexApplicationTests.java
@@ -0,0 +1,13 @@
+package ka15err.aztex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AztexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
